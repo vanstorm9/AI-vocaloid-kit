800324ab55fa573ac29695bcb5e2aebbdedb8307
vanstorm9 <antlowhur@yahoo.com>
2017-09-24 12:25:18 -0500

    inital commit for lyric thing

---
+++ lyric-generation/main.py
@@ -0,0 +1,57 @@
+import support.sylco as sylco
+import support.markovlib as markovlib
+import re
+from string import punctuation
+
+def num_of_words(words):
+        r = re.compile(r'[{}]'.format(punctuation))
+        new_words_cont = r.sub(' ',words)
+        wordlen = len(new_words_cont.split())
+        return wordlen
+
+def splitParagraphIntoSentences(paragraph):
+    ''' break a paragraph into sentences
+        and return a list '''
+    import re
+    # to split by multile characters
+
+    #   regular expressions are easiest (and fastest)
+    sentenceEnders = re.compile('[.!?,]')
+    sentenceList = sentenceEnders.split(paragraph)
+    return sentenceList
+
+
+#print nsyl('multiplication')
+
+#file_ = open('text/combined.txt')
+#file_ = open('scraped_data/1a.txt')
+#file_ = open('text/lyrics.txt')
+
+
+print 'Press any key for love song'
+typ = raw_input()
+
+
+file_ = open('text/combined.txt')
+
+
+while True:
+        markov = markovlib.Markov(file_)
+        text = markov.generate_markov_text()
+        strlen = 0
+        sentences = splitParagraphIntoSentences(text)
+        for s in sentences:
+                senlen = sylco.sylco(s)
+                print s.strip().capitalize()
+        
+
+	print ''
+        print ''
+        
+        print 'Press any key for love song'
+        typ = raw_input()
+
+       	file_ = open('text/combined.txt')
+
+        print ''
+        print ''
---
+++ lyric-generation/support/markovlib.py
@@ -0,0 +1,51 @@
+import random
+
+class Markov(object):
+	
+	def __init__(self, open_file):
+		self.cache = {}
+		self.open_file = open_file
+		self.words = self.file_to_words()
+		self.word_size = len(self.words)
+		self.database()
+		
+	
+	def file_to_words(self):
+		self.open_file.seek(0)
+		data = self.open_file.read()
+		words = data.split()
+		return words
+		
+	
+	def triples(self):
+		""" Generates triples from the given data string. So if our string were
+				"What a lovely day", we'd generate (What, a, lovely) and then
+				(a, lovely, day).
+		"""
+		
+		if len(self.words) < 3:
+			return
+		
+		for i in range(len(self.words) - 2):
+			yield (self.words[i], self.words[i+1], self.words[i+2])
+			
+	def database(self):
+		for w1, w2, w3 in self.triples():
+			key = (w1, w2)
+			if key in self.cache:
+				self.cache[key].append(w3)
+			else:
+				self.cache[key] = [w3]
+				
+	def generate_markov_text(self, size=120):
+		seed = random.randint(0, self.word_size-3)
+		seed_word, next_word = self.words[seed], self.words[seed+1]
+		w1, w2 = seed_word, next_word
+		gen_words = []
+		for i in xrange(size):
+			gen_words.append(w1)
+			w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+		gen_words.append(w2)
+		
+		return ' '.join(gen_words)
+

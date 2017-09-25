dc7d2979efdee5344312c29d6f99b2409e1ed4a3
vanstorm9 <antlowhur@yahoo.com>
2017-09-24 23:10:29 -0500

    More clean up

---
+++ lyric-generation/lyricGenerator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import support.sylco as sylco
+import support.markovlib as markovlib
+import re
+import string
+#from string import punctuation
+
+#txtFilePath = 'text/result.txt'
+#txtFilePath = 'text/romaji.txt'
+txtFilePath = 'text/hiragana.txt'
+
+
+def num_of_words(words):
+        r = re.compile(r'[{}]'.format(punctuation))
+        new_words_cont = r.sub(' ',words)
+        wordlen = len(new_words_cont.split())
+        return wordlen
+
+def splitParagraphIntoSentences(paragraph):
+    ''' break a paragraph into sentences
+        and return a list '''
+    import re
+    # to split by multile characters
+
+    #   regular expressions are easiest (and fastest)
+    sentenceEnders = re.compile('[.!?,]')
+    sentenceList = sentenceEnders.split(paragraph)
+    return sentenceList
+
+
+#print nsyl('multiplication')
+
+#file_ = open('text/combined.txt')
+#file_ = open('scraped_data/1a.txt')
+#file_ = open('text/lyrics.txt')
+
+
+print 'Press any key for love song'
+typ = raw_input()
+
+
+file_ = open(txtFilePath)
+
+
+while True:
+        markov = markovlib.Markov(file_)
+        text = markov.generate_markov_text()
+        sentences = splitParagraphIntoSentences(text)
+        for s in sentences:
+                #senlen = sylco.sylco(s)
+                print s.strip().capitalize()
+		print ''
+        
+        print 'Press any key for love song'
+        typ = raw_input()
+
+       	file_ = open(txtFilePath)
+
+        print ''
+        print ''
--- main.py
+++ main.py
@@ -6,5 +6,10 @@ url = sys.argv[1]
 print 'Converting Youtube video to MIDI. . .'
 ytc.YoutubeToMIDIConvert(url)
 
+## Use lyric generator
+
+
+# Use song composer
+
 print 'Done'
 

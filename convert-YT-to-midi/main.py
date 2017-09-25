63482a13576af7f7713096fed62eacf9f9a1f2e6
vanstorm9 <antlowhur@yahoo.com>
2017-09-24 22:09:37 -0500

    Inserted Youtube to midi converter

---
+++ convert-YT-to-midi/main.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+import youtube_dl
+import os
+
+import music21
+import matplotlib.pyplot as plt
+from midiutil.MidiFile import MIDIFile
+
+from support.noteTranslation import *
+
+import sys,os
+
+skipMidiConversion = False
+
+outputJSON = None
+
+if not skipMidiConversion:
+	# First we convert Youtube video to mp3
+
+	print 'Working with ', sys.argv[1]
+	YT_URL = sys.argv[1]
+
+	outputJSON = sys.argv[2]
+
+	print "Outfile: " + sys.argv[2]
+	print os.getcwd()
+
+	'''
+	print 'Paste the URL of the Youtube video and press [Enter]'
+	YT_URL = raw_input()
+	'''
+	os.system('rm -r bin')
+
+	os.makedirs('./bin')
+
+	ydl_opts = {
+	    'outtmpl': 'bin/output.mp3',
+	    'format': 'bestaudio/best',
+	    'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192',
+	    }],
+	}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	    ydl.download([YT_URL])
+
+
+	# Then we convert mp3 video to midi
+	os.system('python2 ' + os.path.dirname(os.path.realpath(__file__)) + '/support/audio_to_midi_melodia.py bin/output.mp3 bin/output.mid 60 --smooth 0.25 --minduration 0.1 --jams')
+
+# Insert code here to remove delta time
+
+####
+print 'File successfully converted!'
---
+++ convert-YT-to-midi/support/audio_to_midi_melodia.py
@@ -0,0 +1,211 @@
+# CREATED: 11/9/15 3:57 PM by Justin Salamon <justin.salamon@nyu.edu>
+
+import librosa
+import vamp
+import argparse
+import os
+import numpy as np
+from midiutil.MidiFile import MIDIFile
+from scipy.signal import medfilt
+import jams
+import __init__
+
+'''
+Extract the melody from an audio file and convert it to MIDI.
+The script extracts the melody from an audio file using the Melodia algorithm,
+and then segments the continuous pitch sequence into a series of quantized
+notes, and exports to MIDI using the provided BPM. If the --jams option is
+specified the script will also save the output as a JAMS file. Note that the
+JAMS file uses the original note onset/offset times estimated by the algorithm
+and ignores the provided BPM value.
+Note: Melodia can work pretty well and is the result of several years of
+research. The note segmentation/quantization code was hacked in about 30
+minutes. Proceed at your own risk... :)
+usage: audio_to_midi_melodia.py [-h] [--smooth SMOOTH]
+                                [--minduration MINDURATION] [--jams]
+                                infile outfile bpm
+Examples:
+python audio_to_midi_melodia.py --smooth 0.25 --minduration 0.1 --jams
+                                ~/song.wav ~/song.mid 60
+'''
+
+
+def save_jams(jamsfile, notes, track_duration, orig_filename):
+
+    # Construct a new JAMS object and annotation records
+    jam = jams.JAMS()
+
+    # Store the track duration
+    jam.file_metadata.duration = track_duration
+    jam.file_metadata.title = orig_filename
+
+    midi_an = jams.Annotation(namespace='pitch_midi',
+                              duration=track_duration)
+    midi_an.annotation_metadata = \
+        jams.AnnotationMetadata(
+            data_source='audio_to_midi_melodia.py v%s' % __init__.__version__,
+            annotation_tools='audio_to_midi_melodia.py (https://github.com/'
+                             'justinsalamon/audio_to_midi_melodia)')
+
+    # Add midi notes to the annotation record.
+    for n in notes:
+        midi_an.append(time=n[0], duration=n[1], value=n[2], confidence=0)
+
+    # Store the new annotation in the jam
+    jam.annotations.append(midi_an)
+
+    # Save to disk
+    jam.save(jamsfile)
+
+
+def save_midi(outfile, notes, tempo):
+
+    track = 0
+    time = 0
+    midifile = MIDIFile(1)
+
+    # Add track name and tempo.
+    midifile.addTrackName(track, time, "MIDI TRACK")
+    midifile.addTempo(track, time, tempo)
+
+    channel = 0
+    volume = 100
+
+    for note in notes:
+        onset = note[0] * (tempo/60.)
+        duration = note[1] * (tempo/60.)
+        # duration = 1
+        pitch = note[2]
+        midifile.addNote(track, channel, pitch, onset, duration, volume)
+
+    # And write it to disk.
+    binfile = open(outfile, 'wb')
+    midifile.writeFile(binfile)
+    binfile.close()
+
+
+def midi_to_notes(midi, fs, hop, smooth, minduration):
+
+    # smooth midi pitch sequence first
+    if (smooth > 0):
+        filter_duration = smooth  # in seconds
+        filter_size = int(filter_duration * fs / float(hop))
+        if filter_size % 2 == 0:
+            filter_size += 1
+        midi_filt = medfilt(midi, filter_size)
+    else:
+        midi_filt = midi
+    # print(len(midi),len(midi_filt))
+
+    notes = []
+    p_prev = None
+    duration = 0
+    onset = 0
+    for n, p in enumerate(midi_filt):
+        if p == p_prev:
+            duration += 1
+        else:
+            # treat 0 as silence
+            if p_prev > 0:
+                # add note
+                duration_sec = duration * hop / float(fs)
+                # only add notes that are long enough
+                if duration_sec >= minduration:
+                    onset_sec = onset * hop / float(fs)
+                    notes.append((onset_sec, duration_sec, p_prev))
+
+            # start new note
+            onset = n
+            duration = 1
+            p_prev = p
+
+    # add last note
+    if p_prev > 0:
+        # add note
+        duration_sec = duration * hop / float(fs)
+        onset_sec = onset * hop / float(fs)
+        notes.append((onset_sec, duration_sec, p_prev))
+
+    return notes
+
+
+def hz2midi(hz):
+
+    # convert from Hz to midi note
+    hz_nonneg = hz.copy()
+    hz_nonneg[hz <= 0] = 0
+    midi = 69 + 12*np.log2(hz_nonneg/440.)
+    midi[midi <= 0] = 0
+
+    # round
+    midi = np.round(midi)
+
+    return midi
+
+
+def audio_to_midi_melodia(infile, outfile, bpm, smooth=0.25, minduration=0.1,
+                          savejams=False):
+
+    # define analysis parameters
+    fs = 44100
+    hop = 128
+
+    # load audio using librosa
+    print("Loading audio...")
+    data, sr = librosa.load(infile, sr=fs, mono=True)
+
+    # extract melody using melodia vamp plugin
+    print("Extracting melody f0 with MELODIA...")
+    melody = vamp.collect(data, sr, "mtg-melodia:melodia",
+                          parameters={"voicing": 0.2})
+
+    # hop = melody['vector'][0]
+    pitch = melody['vector'][1]
+
+    # impute missing 0's to compensate for starting timestamp
+    pitch = np.insert(pitch, 0, [0]*8)
+
+    # debug
+    # np.asarray(pitch).dump('f0.npy')
+    # print(len(pitch))
+
+    # convert f0 to midi notes
+    print("Converting Hz to MIDI notes...")
+    midi_pitch = hz2midi(pitch)
+
+    # segment sequence into individual midi notes
+    notes = midi_to_notes(midi_pitch, fs, hop, smooth, minduration)
+
+    # save note sequence to a midi file
+    print("Saving MIDI to disk...")
+    save_midi(outfile, notes, bpm)
+
+    if savejams:
+        print("Saving JAMS to disk...")
+        jamsfile = outfile.replace(".mid", ".jams")
+        track_duration = len(data) / float(fs)
+        save_jams(jamsfile, notes, track_duration, os.path.basename(infile))
+
+    print("Conversion complete.")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="Path to input audio file.")
+    parser.add_argument("outfile", help="Path for saving output MIDI file.")
+    parser.add_argument("bpm", type=int, help="Tempo of the track in BPM.")
+    parser.add_argument("--smooth", type=float, default=0.25,
+                        help="Smooth the pitch sequence with a median filter "
+                             "of the provided duration (in seconds).")
+    parser.add_argument("--minduration", type=float, default=0.1,
+                        help="Minimum allowed duration for note (in seconds). "
+                             "Shorter notes will be removed.")
+    parser.add_argument("--jams", action="store_const", const=True,
+                        default=False, help="Also save output in JAMS format.")
+
+    args = parser.parse_args()
+
+    audio_to_midi_melodia(args.infile, args.outfile, args.bpm,
+                          smooth=args.smooth, minduration=args.minduration,
+                          savejams=args.jams)
